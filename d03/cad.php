9459289f169a6918d08e4d097de9062b78f59be3
weversonmp <weversonmnzs@gmail.com>
2023-10-05 10:55:41 -0300

    d03 done

--- d03/cad.php
+++ d03/cad.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Resultado</title>
 </head>
+
 <body>
-    <header>
-        <h1>Resultado do processamento</h1>
-    </header>
 
     <main>
-        <?php 
-        $numero = $_GET["numero"] ?? "sem numero";
-        $antecessor = $numero - 1;
-        $sucessor = $numero + 1;
-        echo "<p>O número escolhido foi <strong>$numero</strong></p>";
-        echo "<p>O seu antecessor é <strong>$antecessor</strong></p>";
-        echo "<p>O seu sucessor é <strong>$sucessor</strong></p>";
+        <h1>Conversor de Moedas v1.0</h1>
+
+        <?php
+        $numero = $_GET["numero"] ?? "sem valor";
+        $real = number_format($numero, 2, ",", ".")  ?? "sem valor";
+        $dolar = number_format($numero / 5.22, 2, ",", ".");
+
+        echo "<p>Seus R$ $numero equivalem a <strong>US$ $dolar</strong></p>";
+        echo "<p><strong>*Cotação fixa de R$ 5,22</strong> informada diretamente no código</p>";
         ?>
         <a href="./index.html"><input type="button" value="⬅️ Voltar"></a>
     </main>
 </body>
+
 </html>
