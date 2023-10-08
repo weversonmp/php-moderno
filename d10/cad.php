97950055ac7b9cae8f2a1aa42aa3716a7dfebde0
weversonmp <weversonmnzs@gmail.com>
2023-10-08 20:42:59 -0300

    d10 done

--- d10/cad.php
+++ d10/cad.php
@@ -17,33 +17,29 @@
     ?>
 
     <main>
-        <h1>Média Aritméticas</h1>
+        <h1>Calculando sua idade</h1>
         <form action="<?= $_SERVER["PHP_SELF"] ?>" method="get">
-            <label for="v1">1º Valor</label>
-            <input type="number" name="v1" id="v1" require value="<?=$valor1?>">
-            <label for="p1">1º Peso</label>
-            <input type="number" name="p1" id="p1" min="1" require value="<?=$peso1?>">
-            <label for="v2">2º Valor</label>
-            <input type="number" name="v2" id="v2" require value="<?=$valor2?>">
-            <label for="p2">2º Peso</label>
-            <input type="number" name="p2" id="p2" min="1" require value="<?=$peso2?>">
+            <label for="nasc">Em que ano você nasceu?</label>
+            <input type="number" name="nasc" id="nasc" min="1900" require value="<?= $valor1 ?>">
 
-            <input type="submit" value="Calcular Médias">
+            <label for="ano">1º Peso</label>
+            <input type="number" name="ano" id="ano" min="1900" require value="<?= $peso1 ?>">
+
+            <input type="submit" value="Qual será minha idade?">
 
         </form>
     </main>
     <section>
-        <?php 
+        <?php
         $ma = ($valor1 + $valor2) / 2;
-        $mp = (($peso1 * $valor1)+($peso2*$valor2))/($peso1+$peso2);
+        $mp = (($peso1 * $valor1) + ($peso2 * $valor2)) / ($peso1 + $peso2);
         ?>
-        
-        <h2>Cálculo das Médias</h2>
-        <p>Analisando os valores {} e {}:</p>
+
+        <h2>Resultado</h2>
 
         <ul>
-            <li>A <strong>Média Aritmética Simples</strong> entre os valores é igual a <?=number_format($ma, 2, ",", ".")?></li>
-            <li>A <strong>Média Aritmética Ponderada</strong> com pesos <?=$peso1 . " e " . $peso2?> é igual a <?=number_format($mp, 2, ",", ".")?>.</li>
+            <li>A <strong>Média Aritmética Simples</strong> entre os valores é igual a <?= number_format($ma, 2, ",", ".") ?></li>
+            <li>A <strong>Média Aritmética Ponderada</strong> com pesos <?= $peso1 . " e " . $peso2 ?> é igual a <?= number_format($mp, 2, ",", ".") ?>.</li>
         </ul>
     </section>
 
